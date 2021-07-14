a410e1b1aa10771e1eb808d6a2dc42e2cf9141c4
Hashan <hashan2@hotmail.com>
2021-07-14 15:46:36 +0100

    Basic SPI RX TX Test Success

--- SPIDriver/Program.cs
+++ SPIDriver/Program.cs
@@ -10,6 +10,18 @@ namespace SPIDriver
         {
 		Core.WiringPiSetup();
 		var spi = new SPI(0, 500000);
+		var testBytes = new byte[] {0xF4, 0xA0, 0x33};
+		DumpHex(testBytes);
+		var ret = spi.SPIRxTx(testBytes);
+		DumpHex(ret);
         }
+
+
+	private static void DumpHex(byte[] bytes){
+		for(int i = 0; i < bytes.Length; i++){
+			Console.Write($"{bytes[i].ToString("X2")} ");
+		}
+		Console.WriteLine();
+	}
     }
 }
